f3e64f20913c746255c4e5942e1e750c5bb115ff
Sai Subham Jena <jenasaisubham8@gmail.com>
2025-10-29 23:41:22 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -74,22 +74,23 @@ const Footer: React.FC = () => {
 
         {/* Divider + Copyright */}
          <div className="border-t border-white/40 mt-10 pt-4 text-center text-sm text-[#fff8e1]">
-          © {new Date().getFullYear()}{" "}
-          <span className="font-semibold text-white">Nashville Hanuman Temple</span>. All
-          rights reserved.
-          <br />
-          <span className="text-white text-xs mt-1 inline-block">
-            Made with ❤️ by{" "}
-            <a
-              href="https://elite8digital.in/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-semibold hover:underline"
-            >
-              Elite8Digital
-            </a>
-          </span>
-        </div>
+  © {new Date().getFullYear()}{" "}
+  <span className="font-semibold text-white">Nashville Hanuman Temple</span>. All
+  rights reserved.
+  <br />
+  <span className="text-white text-xs mt-1 inline-block">
+    Made with ❤️ by{" "}
+    <a
+      href="https://elite8digital.in/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-semibold text-blue-500"
+    >
+      Elite8Digital
+    </a>
+  </span>
+</div>
+
       </div>
     </footer>
   );
